d15f4cdf63f19bc7873e064591292a8593066a39
Philipp Schlegel <observing@web.de>
2022-03-17 11:35:06 +0100

    add new variant: nblast_sparse

--- README.md
+++ README.md
@@ -1,27 +1,30 @@
 # NBLAST for large datasets
-This is an experimental version of [navis](https://github.com/navis-org/navis)'
+Experimental version of [navis](https://github.com/navis-org/navis)'
 NBLAST containing a few tweaks to enable blasting of very large (upwards 100k)
-sets of neurons. Notably, we:
+sets of neurons.
+
+> :exclamation: **If you need to NBLAST only a few thousand neurons, you are probably better off with the implementation in navis**!
+
+At this point we offer two variants:
+
+## NBLAST on disk
+This implementation immediately writes scores to disk. Notably, we:
 
 1. Use [Zarr](https://zarr.readthedocs.io) to create an on-disk array which is then populated in parallel by the spawned NBLAST processes.
 2. Use [Dask](https://docs.dask.org) to wrap the `Zarr` array as on-disk DataFrame.
 3. Some (minimal) effort to balance the load for each process.
 
-> :exclamation: **If you need to NBLAST only a few thousand neurons, you are probably better off with the implementation in navis**!
-
-## Usage
-
 ```python
->>> import nblast
+>>> from nblast import nblast_disk
 >>> # `queries`/`targets` are navis.Dotprops
->>> scores = nblast.nblast_disk(queries, targets,
-...                             out='./nblast_results'  # where to store Zarr array with results
-...                             return_frame=True)      # default is `return_frame=False`
+>>> scores = .nblast_disk(queries, targets,
+...                       out='./nblast_results'  # where to store Zarr array with results
+...                       return_frame=True)      # default is `return_frame=False`
 >>> # Convert Dask to in-memory pandas DataFrame
 >>> scores = scores.compute()
 ```
 
-The scores are stored as Zarr array in `./nblast_results/scores`:
+The scores are stored as Zarr array in `./nblast_results/scores`. To re-open:
 
 ```python
 >>> import zarr
@@ -32,12 +35,27 @@ The scores are stored as Zarr array in `./nblast_results/scores`:
 [5812980561, 5813056261, 1846312261, 2373845661, ...]
 ```
 
-## Notes
-
 ### Why Zarr for the on-disk array?
 Because Zarr has a very easy to use interface for coordinating parallel writes.
 
 ### Is this slower than the original navis implementation?
 Not as far as I can tell: the overhead from using Zarr/Dask appears surprisingly
 low. It does, however, introduce new dependencies and the returned Dask
-DataFrame is more difficult to handle than a normal pandas DataFrame.
+DataFrame is more difficult to handle than run-of-the-mill pandas DataFrames.
+
+
+## NBLAST to sparse matrix
+This implementation drops scores below a given threshold and stores results in
+a sparse matrix. This is useful if you don't care about low (say less than 0)
+scores.
+
+```python
+>>> from nblast import nblast_sparse
+>>> # `queries`/`targets` are navis.Dotprops
+>>> scores = nblast_sparse(queries, targets, threshold=0)
+```
+
+`scores` is a pandas DataFrame where each column is a sparse array. It will,
+in many ways, handle like a normal DataFrame. See the
+[docs](https://pandas.pydata.org/pandas-docs/stable/user_guide/sparse.html) for
+details.
--- nblast/base.py
+++ nblast/base.py
@@ -11,6 +11,7 @@ import dask.array as da
 import dask.dataframe as dd
 import numpy as np
 import pandas as pd
+import scipy.sparse as sp
 
 from concurrent.futures import ProcessPoolExecutor
 from typing import Union, Optional, List
@@ -27,12 +28,13 @@ Dotprops = navis.Dotprops
 NeuronList = navis.NeuronList
 
 
-class DiskBlaster(navis.nbl.nblast_funcs.NBlaster):
-    def __init__(self, out, offset=(0, 0), *args, **kwargs):
+class LargeBlaster(navis.nbl.nblast_funcs.NBlaster):
+    """Base class for large NBLASTs."""
+
+    def __init__(self, offset=(0, 0), *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.neurons_ds = []
         self.self_hits_ds = []
-        self.out = out
         self.offset = offset
 
     def append(self, dotprops, downsampled=None):
@@ -100,11 +102,18 @@ class DiskBlaster(navis.nbl.nblast_funcs.NBlaster):
 
         return scr
 
-    def multi_query_target_disc(self, q_idx, t_idx,
-                                chunksize=10,
-                                scores='forward',
-                                smart=False):
-        """BLAST multiple queries against multiple targets - on disk.
+
+class DiskBlaster(LargeBlaster):
+    """NBLAST to disk."""
+    def __init__(self, out, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.out = out
+
+    def multi_query_target(self, q_idx, t_idx,
+                          chunksize=10,
+                          scores='forward',
+                          smart=False):
+        """BLAST multiple queries against multiple targets.
 
         Parameters
         ----------
@@ -180,6 +189,93 @@ class DiskBlaster(navis.nbl.nblast_funcs.NBlaster):
         return
 
 
+class SparseBlaster(LargeBlaster):
+    """NBLAST to sparse matrix (coo/csc format)."""
+    def __init__(self, threshold, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.threshold = threshold
+
+    def multi_query_target(self, q_idx, t_idx,
+                          chunksize=10,
+                          scores='forward',
+                          smart=False):
+        """BLAST multiple queries against multiple targets.
+
+        Parameters
+        ----------
+        q_idx,t_idx :       iterable
+                            Iterable of query/target neuron indices to BLAST.
+        chunksize :         int
+                            Data will be written to the array in chunks of this
+                            size.
+        scores :            "forward" | "mean" | "min" | "max"
+                            Which scores to return.
+        smart :             bool
+                            If True will run a pre-NBLAST on the downsampled
+                            dotprops and then only full NBLAST the top 10% for
+                            each query.
+
+        Returns
+        -------
+        None
+
+        """
+        # Top 10% of neurons to take to full NBLAST
+        top_N = len(t_idx) // 10
+
+        if utils.is_jupyter() and config.tqdm == config.tqdm_notebook:
+            # Jupyter does not like the progress bar position for some reason
+            position = None
+
+            # For some reason we have to do this if we are in a Jupyter environment
+            # and are using multi-processing because otherwise the progress bars
+            # won't show. See this issue:
+            # https://github.com/tqdm/tqdm/issues/485#issuecomment-473338308
+            print(' ', end='', flush=True)
+        else:
+            position = getattr(self, 'pbar_position', 0)
+
+        # Create empty chunk
+        all_scores = []
+        all_coo = []
+        row_offset, col_offset = self.offset  # shorthands
+        for i, q in enumerate(config.tqdm(q_idx,
+                                          desc=self.desc,
+                                          leave=False,
+                                          position=position,
+                                          disable=not self.progress)):
+            this_scores = []
+            this_coo = []
+            this_t = []
+            for k, t in enumerate(t_idx):
+                if not smart:
+                    s = self.single_query_target(q, t, scores=scores)
+                else:
+                    s = self.single_query_target_ds(q, t, scores=scores)
+
+                if s >= self.threshold:
+                    this_scores.append(s)
+                    this_coo.append([i, k])
+                    this_t.append(t)
+
+            if smart and len(this_scores):
+                # Run full NBLAST for the top 10%
+                for k in np.argsort(this_scores)[-top_N:]:
+                    this_scores[k] = self.single_query_target(q, this_t[k], scores=scores)
+            all_scores += this_scores
+            all_coo += this_coo
+
+        all_scores = np.array(all_scores, dtype=self.dtype)
+        all_coo = np.array(all_coo, dtype=np.int32)
+
+        if len(all_coo):
+            all_coo += self.offset
+        else:
+            all_coo = np.zeros((0, 2), dtype=np.int32)
+
+        return all_scores, all_coo
+
+
 def nblast_disk(query: Union[Dotprops, NeuronList],
                 target: Optional[str] = None,
                 out: str = None,
@@ -196,7 +292,7 @@ def nblast_disk(query: Union[Dotprops, NeuronList],
                 dtype: Union[str, np.dtype] = 'float32',
                 return_frame: bool = False,
                 progress: bool = True) -> pd.DataFrame:
-    """NBLAST query against target neurons.
+    """NBLAST query against target neurons and write results to disk.
 
     Parameters
     ----------
@@ -381,14 +477,14 @@ def nblast_disk(query: Union[Dotprops, NeuronList],
 
     # If only one core, we don't need to break out the multiprocessing
     if n_cores == 1:
-        _ = this.multi_query_target_disc(this.queries,
-                                         this.targets,
-                                         smart=smart,
-                                         scores=scores)
+        _ = this.multi_query_target(this.queries,
+                                    this.targets,
+                                    smart=smart,
+                                    scores=scores)
     else:
         with ProcessPoolExecutor(max_workers=n_cores) as pool:
             # Each nblaster is passed to its own process
-            futures = [pool.submit(this.multi_query_target_disc,
+            futures = [pool.submit(this.multi_query_target,
                                    q_idx=this.queries,
                                    t_idx=this.targets,
                                    smart=smart,
@@ -406,6 +502,199 @@ def nblast_disk(query: Union[Dotprops, NeuronList],
         scores = scores[sorted(scores.columns)]
 
         return scores
+
+
+def nblast_sparse(query: Union[Dotprops, NeuronList],
+                  target: Optional[str] = None,
+                  threshold: float = 0,
+                  scores: Union[Literal['forward'],
+                                Literal['mean'],
+                                Literal['min'],
+                                Literal['max']] = 'forward',
+                  normalized: bool = True,
+                  smart: bool = False,
+                  use_alpha: bool = False,
+                  smat: Optional[Union[str, pd.DataFrame]] = 'auto',
+                  limit_dist: Optional[Union[Literal['auto'], int, float]] = None,
+                  n_cores: int = os.cpu_count() // 2,
+                  dtype: Union[str, np.dtype] = 'float32',
+                  progress: bool = True) -> pd.DataFrame:
+    """NBLAST query against target neurons and store results as sparse matrix.
+
+    Parameters
+    ----------
+    query :         Dotprops | NeuronList
+                    Query neuron(s) to NBLAST against the targets. Neurons
+                    should be in microns as NBLAST is optimized for that and
+                    have similar sampling resolutions.
+    target :        Dotprops | NeuronList, optional
+                    Target neuron(s) to NBLAST against. Neurons should be in
+                    microns as NBLAST is optimized for that and have
+                    similar sampling resolutions. If not provided, will NBLAST
+                    queries against themselves.
+    threshold :     float
+                    NBLAST scores below this will be ignored and not stored.
+                    If this value is too low (i.e. to few values get dropped)
+                    the resulting sparse matrix might turn out using more memory
+                    than a corresponding dense matrix.
+    scores :        'forward' | 'mean' | 'min' | 'max'
+                    Determines the final scores:
+                      - 'forward' (default) returns query->target scores
+                      - 'mean' returns the mean of query->target and
+                        target->query scores
+                      - 'min' returns the minium between query->target and
+                        target->query scores
+                      - 'max' returns the maximum between query->target and
+                        target->query scores
+    normalized :    bool, optional
+                    Whether to return normalized NBLAST scores.
+    smart :         bool
+                    If True, will NBLAST downsampled dotprops first and, for
+                    each query, take only the top 10% forward for a full NBLAST.
+    use_alpha :     bool, optional
+                    Emphasizes neurons' straight parts (backbone) over parts
+                    that have lots of branches.
+    smat :          str | pd.DataFrame
+                    Score matrix. If 'auto' (default), will use scoring matrices
+                    from FCWB. Same behaviour as in R's nat.nblast
+                    implementation. If ``smat=None`` the scores will be
+                    generated as the product of the distances and the dotproduct
+                    of the vectors of nearest-neighbor pairs.
+    limit_dist :    float | "auto" | None
+                    Sets the max distance for the nearest neighbor search
+                    (`distance_upper_bound`). Typically this should be the
+                    highest distance considered by the scoring function. If
+                    "auto", will extract that value from the scoring matrix.
+    n_cores :       int, optional
+                    Max number of cores to use for nblasting. Default is
+                    ``os.cpu_count() // 2``. This should ideally be an even
+                    number as that allows optimally splitting queries onto
+                    individual processes.
+    dtype :         str [e.g. "float32"] | np.dtype
+                    Precision for scores. Defaults to 32 bit (single) floats.
+                    This is useful to reduce the memory footprint for very large
+                    matrices. In real-world scenarios 32 bit (single)- and
+                    depending on the purpose even 16 bit (half) - are typically
+                    sufficient.
+    progress :      bool
+                    Whether to show progress bars.
+
+    References
+    ----------
+    Costa M, Manton JD, Ostrovsky AD, Prohaska S, Jefferis GS. NBLAST: Rapid,
+    Sensitive Comparison of Neuronal Structure and Construction of Neuron
+    Family Databases. Neuron. 2016 Jul 20;91(2):293-311.
+    doi: 10.1016/j.neuron.2016.06.012.
+
+    Returns
+    -------
+    pandas.DataFrame
+                    Where each column is a sparse COO-array.
+
+    """
+    # Make sure we're working on NeuronLists
+    query_dps = NeuronList(query)
+    # We will shuffle to avoid having imbalanced loading of the NBLASTERs
+    random.shuffle(query_dps.neurons)
+
+    if smart:
+        query_ds = navis.downsample_neuron(query_dps, 10,
+                                           parallel=n_cores > 1,
+                                           n_cores=n_cores)
+    else:
+        query_ds = None
+
+    aba = False
+    if isinstance(target, type(None)):
+        aba = True
+        target, target_dps, target_ds = query, query_dps, query_ds
+    else:
+        target_dps = NeuronList(target)
+        random.shuffle(target_dps.neurons)
+
+        if smart:
+            target_ds = navis.downsample_neuron(target_dps, 10,
+                                                parallel=n_cores > 1,
+                                                n_cores=n_cores)
+
+    # Run NBLAST preflight checks
+    navis.nbl.nblast_funcs.nblast_preflight(query_dps, target_dps,
+                                            n_cores,
+                                            req_unique_ids=True)
+
+    # Find an optimal partition that minimizes the number of neurons
+    # we have to send to each process
+    n_rows, n_cols = navis.nbl.nblast_funcs.find_optimal_partition(n_cores,
+                                                                   query_dps,
+                                                                   target_dps)
+
+    nblasters = []
+    with config.tqdm(desc='Preparing',
+                     total=n_rows * n_cols,
+                     leave=False,
+                     disable=not progress) as pbar:
+        query_idx = np.arange(len(query_dps))
+        target_idx = np.arange(len(target_dps))
+        offset = [0, 0]
+        for q in np.array_split(query_idx, n_rows):
+            offset[1] = 0
+            for t in np.array_split(target_idx, n_cols):
+                # Initialize NBlaster
+                this = SparseBlaster(use_alpha=use_alpha,
+                                     normalized=normalized,
+                                     smat=smat,
+                                     limit_dist=limit_dist,
+                                     dtype=dtype,
+                                     offset=tuple(offset),  # need to make copy here
+                                     threshold=threshold,
+                                     progress=progress)
+
+                # Add queries and target
+                this.append(query_dps[q],
+                            downsampled=None if not smart else query_ds[q])
+                this.append(target_dps[t],
+                            downsampled=None if not smart else target_dps[t])
+
+                # Keep track of indices of queries and targets
+                this.queries = np.arange(len(q))
+                this.targets = np.arange(len(t)) + len(q)
+                this.pbar_position = len(nblasters) % n_cores
+
+                offset[1] += len(t)
+
+                nblasters.append(this)
+                pbar.update(1)
+
+            offset[0] += len(q)
+
+    # If only one core, we don't need to break out the multiprocessing
+    if n_cores == 1:
+        data, coo = this.multi_query_target(this.queries,
+                                              this.targets,
+                                              smart=smart,
+                                              scores=scores)
+    else:
+        with ProcessPoolExecutor(max_workers=n_cores) as pool:
+            # Each nblaster is passed to its own process
+            futures = [pool.submit(this.multi_query_target,
+                                   q_idx=this.queries,
+                                   t_idx=this.targets,
+                                   smart=smart,
+                                   scores=scores) for this in nblasters]
+
+            results = [f.result() for f in futures]
+
+            data = np.concatenate([r[0] for r in results], dtype=dtype)
+            coo = np.vstack([r[1] for r in results])
+
+    scores = sp.csc_array((data, (coo[:, 0], coo[:, 1])))
+
+    scores = pd.DataFrame.sparse.from_spmatrix(scores,
+                                               index=query_dps.id,
+                                               columns=target_dps.id)
+    scores = scores.astype(pd.SparseDtype(dtype, np.nan))
+
+    return scores
 
 
 def matches(query: Union[Dotprops, NeuronList],
